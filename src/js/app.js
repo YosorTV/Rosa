273212c105a39ce2b266004f380483a0cd465923
YosorIT <yosorit@gmail.com>
2020-03-04 01:02:48 +0200

    header&menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
 import scrollreveal from 'scrollreveal';
+
+const selectElement = (element) => document.querySelector(element);
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', ()=> body.classList.toggle('open'));
